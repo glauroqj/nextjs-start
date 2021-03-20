7211d05cdc09679a57be45dc82c1a53d8c07ed7a
glauroqj <glauro.juliani@hotmail.com>
2021-03-20 10:50:02 -0300

    add: new folders

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+const runMiddleware = req => new Promise((resolve) => {
+
+  setTimeout(() => {
+
+    console.log(req.url)
+    resolve()
+
+  }, 5000)
+
+})
+
+export default async (req, res) => {
+
+  await runMiddleware(req)
+
+  res.status(200).json({ name: 'John Doe - Maneiro' })
 }
