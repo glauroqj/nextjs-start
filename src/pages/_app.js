141737efe5385c3baa66e15d2b2e741dafba752c
glauroqj <glauro.juliani@hotmail.com>
2021-03-20 10:57:48 -0300

    add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
+/** MAIN FILE */
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
